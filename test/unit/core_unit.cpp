4b0a978bd9dfbef3595540c420973a50cd9ea527
cam <camille.lagaillardie@gadz.org>
2021-11-18 14:14:13 +0100

    Gtest integration with conan

--- test/unit/core_unit.cpp
+++ test/unit/core_unit.cpp
@@ -1,12 +1,26 @@
 #include "gtest/gtest.h"
 
-// class CoreTest : public ::testing::Test
-// {
+#include "lib/core/Registry.h"
 
-// };
-
-TEST(CoreTest, SimpleTest)
+class CoreTest : public ::testing::Test
 {
-    int i = 0;
-    ASSERT_EQ(i, 0);
+public:
+    static cement::Registry m_registry;
+};
+
+cement::Registry CoreTest::m_registry;
+
+TEST_F(CoreTest, SimpleTest)
+{
+    auto size = m_registry.CreateProperty<long>("Size");
+    auto color = m_registry.CreateProperty<std::string>("Color");
+    auto candle = m_registry.CreateModel("Candle");
+    candle->AddProperty(size);
+    candle->AddProperty(color);
+    auto candle1 = candle->Instanciate();
+    auto candle2 = candle->Instanciate();
+    candle->SetValue<long>(size, candle1, 32);
+    candle->SetValue<long>(size, candle2, 10);
+    candle->SetValue<std::string>(color, candle1, "Blue");
+    candle->SetValue<std::string>(color, candle2, "Red");
 }
