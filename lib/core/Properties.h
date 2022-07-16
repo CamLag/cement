623bbd1dc4ddc45fda99a6e7a039d32c5707ce25
CamilleLag <camille.lagaillardie@gadz.org>
2022-07-16 17:30:10 +0200

    difinitions of properties class members moved in cpp

--- lib/core/Properties.h
+++ lib/core/Properties.h
@@ -8,70 +8,21 @@ namespace cement
     class Properties : public Property
     {
     public:
-        Properties(size_t a_id) : Property(a_id, "Properties", true)
-        {
-        }
+        Properties(size_t a_id);
 
-        size_t AddProperty(Property* a_property)
-        {
-            m_properties[a_property->m_id] = a_property;
-        }
+        size_t AddProperty(Property* a_property);
 
-        Property* GetProperty(size_t a_id) const
-        {
-            auto it = m_properties.find(a_id);
-            if (it == m_properties.end())
-            {
-                return nullptr;
-            }
+        Property* GetProperty(size_t a_id) const;
 
-            return it->second;
-        }
+        virtual void Get(size_t a_id, std::string &a_string_value) override;
 
-        virtual void Get(size_t a_id, std::string &a_string_value) override
-        {
-            auto prop = GetProperty(a_id);
-            a_string_value = prop->Print();
-        }
+        virtual void GetPointedValue(size_t a_instance, std::string &a_string_value) override;
 
-        virtual void GetPointedValue(size_t a_instance, std::string &a_string_value) override
-        {
-            Get(a_instance, a_string_value);
-        }
+        virtual void InternalDeleteInstance(size_t a_id) override;
 
-        virtual void InternalDeleteInstance(size_t a_id) override
-        {
-            auto it = m_properties.find(a_id);
-            if (it == m_properties.end())
-            {
-                return;
-            }
-            m_properties.erase(it);
-        }
+        virtual size_t Size() const override;
 
-        virtual size_t Size() const override
-        {
-            return m_properties.size();
-        }
-
-        virtual std::string Print() const override
-        {
-            std::string result;
-            result += Property::Print();
-            result += " [";
-
-            for (auto elem : m_properties)
-            {
-                result += elem.second->Print();
-                result += ", ";
-            }
-
-            result.pop_back();
-            result.pop_back();
-            result += "]\n";
-
-            return result;
-        }
+        virtual std::string Print() const override;
 
         virtual PropertyType Type() const override;
 
--- lib/core/src/Properties.cpp
+++ lib/core/src/Properties.cpp
@@ -2,6 +2,72 @@
 
 namespace cement
 {
+    Properties::Properties(size_t a_id) : Property(a_id, "Properties", true)
+    {
+    }
+
+    size_t Properties::AddProperty(Property *a_property)
+    {
+        m_properties[a_property->m_id] = a_property;
+        return a_property->m_id;
+    }
+
+    Property *Properties::GetProperty(size_t a_id) const
+    {
+        auto it = m_properties.find(a_id);
+        if (it == m_properties.end())
+        {
+            return nullptr;
+        }
+
+        return it->second;
+    }
+
+    void Properties::Get(size_t a_id, std::string &a_string_value)
+    {
+        auto prop = GetProperty(a_id);
+        a_string_value = prop->Print();
+    }
+
+    void Properties::GetPointedValue(size_t a_instance, std::string &a_string_value)
+    {
+        Get(a_instance, a_string_value);
+    }
+
+    void Properties::InternalDeleteInstance(size_t a_id)
+    {
+        auto it = m_properties.find(a_id);
+        if (it == m_properties.end())
+        {
+            return;
+        }
+        m_properties.erase(it);
+    }
+
+    size_t Properties::Size() const
+    {
+        return m_properties.size();
+    }
+
+    std::string Properties::Print() const
+    {
+        std::string result;
+        result += Property::Print();
+        result += " [";
+
+        for (auto elem : m_properties)
+        {
+            result += elem.second->Print();
+            result += ", ";
+        }
+
+        result.pop_back();
+        result.pop_back();
+        result += "]\n";
+
+        return result;
+    }
+
     PropertyType cement::Properties::Type() const
     {
         return pt_property;
